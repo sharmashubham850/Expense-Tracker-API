37643c2c5404f76705adb88e59efcdfedfc5caea
Shubham Sharma <sharmashubham850@gmail.com>
2022-10-04 09:36:07 +0530

    Implemented login and register controller endpoints

--- src/main/java/com/shubbi/expensetracker/controllers/CategoryController.java
+++ src/main/java/com/shubbi/expensetracker/controllers/CategoryController.java
@@ -1,5 +1,6 @@
 package com.shubbi.expensetracker.controllers;
 
+import com.shubbi.expensetracker.exceptions.EtAuthException;
 import com.shubbi.expensetracker.models.Category;
 import com.shubbi.expensetracker.models.User;
 import com.shubbi.expensetracker.repositories.CategoryRepository;
@@ -15,6 +16,8 @@ public class CategoryController {
 
     private CategoryRepository categoryRepository;
     private UserRepository userRepository;
+
+    private final Integer LOGGED_IN_USER_ID=1;
 
     @Autowired
     public CategoryController(CategoryRepository categoryRepository, UserRepository userRepository){
@@ -32,8 +35,8 @@ public class CategoryController {
 
     @PostMapping("")
     public Category addCategory(@RequestBody Category category){
-        User loggedInUser = userRepository.findById(1).orElseThrow(
-                () -> new RuntimeException("No Logged In User found")
+        User loggedInUser = userRepository.findById(LOGGED_IN_USER_ID).orElseThrow(
+                () -> new EtAuthException("No Logged In User found")
         );
 
         category.setUser(loggedInUser);
--- src/main/java/com/shubbi/expensetracker/controllers/UserController.java
+++ src/main/java/com/shubbi/expensetracker/controllers/UserController.java
@@ -1,21 +1,45 @@
 package com.shubbi.expensetracker.controllers;
 
+import com.shubbi.expensetracker.exceptions.EtAuthException;
+import com.shubbi.expensetracker.exceptions.UserNotFoundException;
 import com.shubbi.expensetracker.models.User;
 import com.shubbi.expensetracker.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import java.util.*;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
 
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
+
 
     @Autowired
     public UserController(UserRepository userRepository){
         this.userRepository = userRepository;
+    }
+
+    @PostMapping("/register")
+    public User registerUser(@RequestBody User user){
+        User savedUser = userRepository.save(user);
+        return savedUser;
+    }
+
+    @PostMapping("/login")
+    public Map<String, String> loginUser(@RequestBody Map<String, Object> userMap){
+        String email = (String) userMap.get("email");
+        String password = (String) userMap.get("password");
+
+        User u = userRepository.validate(email.toLowerCase());
+
+        if (u == null) throw new EtAuthException("No user found with email: "+ email);
+        if (!u.getPassword().equals(password)) throw new EtAuthException("Invalid password");
+
+        Map<String, String> map = new HashMap<>();
+        map.put("success", "true");
+        return map;
     }
 
     @GetMapping("")
@@ -23,11 +47,6 @@ public class UserController {
         return userRepository.findAll();
     }
 
-    @PostMapping("")
-    public User addUser(@RequestBody User user){
-        User savedUser = userRepository.save(user);
-        return savedUser;
-    }
 
     @GetMapping("/{id}")
     public User getUser(@PathVariable("id") Integer userId){
